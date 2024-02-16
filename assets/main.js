b2643e8eeb03b45aaf64b226dbb06de022fe1338
Joaquin <joaquinxavier97@hotmail.com>
2024-02-16 20:12:10 -0300

    add links header

--- assets/main.js
+++
@@ -1,8 +0,0 @@
-
-const menu = document.querySelector('.btn')
-const headerlinks = document.querySelector('.headerlinks')
-
-menu.addEventListener('click', () => {
-    headerlinks.classList.toggle('activado')
-});
-
